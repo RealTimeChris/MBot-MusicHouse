5439b10ae98c1ac32fac8cc70fdce0df2d28d248
unknown <40668522+RealTimeChris@users.noreply.github.com>
2021-04-07 18:02:09 -0400

    Updates!

--- ToCompile/commands/botinfo.ts
+++ ToCompile/commands/botinfo.ts
@@ -25,25 +25,22 @@ async function execute(commandData: FoundationClasses.CommandData, discordUser: 
        const commandReturnData: FoundationClasses.CommandReturnData = {
            commandName: command.name
        };
-
-       const guildData = new GuildData({dataBase: discordUser.dataBase, name: commandData.guild!.name, memberCount: commandData.guild!.memberCount, id: commandData.guild!.id});
-       await guildData.getFromDataBase();
-
+       
        if (commandData.args[0]?.toLowerCase() !== 'janny' && commandData.args[0]?.toLowerCase() !== 'musichouse' && commandData.args[0]?.toLowerCase() !== 'gamehouse'){
-           const msgString = '------\n**Please, enter the name of a bot as the first argument! (!displayguildsdata = BOTNAME)**\n------'
-           const msgEmbed = new Discord.MessageEmbed();
-           msgEmbed
-               .setAuthor((commandData.guildMember as Discord.GuildMember).user.username, (commandData.guildMember as Discord.GuildMember).user.avatarURL()!)
-               .setColor(guildData.borderColor)
-               .setDescription(msgString)
-               .setTimestamp(Date() as unknown as Date)
-               .setTitle("__**Invalid Or Missing Arguments:**__")
-           let msg = await HelperFunctions.sendMessageWithCorrectChannel(commandData, msgEmbed);
-           if (commandData.toTextChannel instanceof Discord.WebhookClient){
-               msg = new Discord.Message(commandData.guild!.client, msg, commandData.fromTextChannel!);
-           }
-           await msg.delete({timeout: 20000});
-       }
+        const msgString = '------\n**Please, enter the name of a bot as the first argument! (!displayguildsdata = BOTNAME)**\n------'
+        const msgEmbed = new Discord.MessageEmbed();
+        msgEmbed
+            .setAuthor(discordUser.userData.userName, commandData.guildMember?.client.users.resolve(discordUser.userData.userID)?.avatarURL()!)
+            .setColor([254, 254, 254])
+            .setDescription(msgString)
+            .setTimestamp(Date() as unknown as Date)
+            .setTitle("__**Invalid Or Missing Arguments:**__")
+        let msg = await HelperFunctions.sendMessageWithCorrectChannel(commandData, msgEmbed);
+        if (commandData.toTextChannel instanceof Discord.WebhookClient){
+            msg = new Discord.Message(commandData.guild!.client, msg, commandData.fromTextChannel!);
+        }
+        await msg.delete({timeout: 20000});
+    }
        if (commandData.args[0]?.toLowerCase() !== 'musichouse'){
            return commandReturnData;
        }
